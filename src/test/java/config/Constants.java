e5f48c79899139dd981d962dce01b1f9bba167ef
AkashPatel007 <43925618+AkashPatel007@users.noreply.github.com>
2018-11-13 23:14:59 +0530

    config file change

--- src/test/java/config/Constants.java
+++ src/test/java/config/Constants.java
@@ -2,15 +2,20 @@ package config;
 
 public class Constants 
 {
+	
+		String currentDir = System.getProperty("user.dir");
+		
 		// URL of the application
 		public String URL= "https://demo.opencart.com/";
-		
+		//%system.teamcity.build.workingDir%
+		//D:/ashu/Automation/mavenartid/ExcelInputFile/NewTest1.xls
+		//currentDir = D:/ashu/Automation/mavenartid
 		// ExcelInput file folder location
-		public String ExcelInputFilePath_NewTest1 = "D:/ashu/Automation/mavenartid/ExcelInputFile/NewTest1.xls";
-		public String ORFolderPath = "D:/ashu/Automation/mavenartid/src/test/java/config/";
-		public String ExtentreportFolderPath = "D:/ashu/Automation/mavenartid/Output/ExtentReportFolder/";
-		public String ScreenshotFolderPath = "D:/ashu/Automation/mavenartid/Output/test/";
-		public String RootFolderPath = "D:/ashu/Automation/mavenartid/";
+		public String ExcelInputFilePath_NewTest1 = currentDir+"/ExcelInputFile/NewTest1.xls";
+		public String ORFolderPath = currentDir+"/src/test/java/config/";
+		public String ExtentreportFolderPath = currentDir+"/Output/ExtentReportFolder/";
+		public String ScreenshotFolderPath = currentDir+"/Output/test/";
+		public String RootFolderPath = currentDir+"/";
 		public String Browser = "chrome";
 		
 }
